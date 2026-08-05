2f44336e3d00c68c5a6923801f6be032c56419bd
varshasu2023-prog <varshasu2023@gmail.com>
2026-08-05 13:23:02 +0530

    Update text_splitter.py

--- utils/text_splitter.py
+++ utils/text_splitter.py
@@ -1,11 +1,13 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-def split_text(text):
+
+def split_documents(documents):
+
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=200
     )
 
-    chunks = splitter.create_documents([text])
+    chunks = splitter.split_documents(documents)
 
     return chunks
