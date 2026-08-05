8cf91a00e6831c6ab2001ebd82b26ba7dfa05dda
varshasu2023-prog <varshasu2023@gmail.com>
2026-08-05 11:41:29 +0530

    Update retriever.py

--- utils/retriever.py
+++ utils/retriever.py
@@ -1,1 +1,12 @@
+from vector_db.chroma_store import load_vector_store
 
+def retrieve_documents(question):
+    db = load_vector_store()
+
+    retriever = db.as_retriever(
+        search_kwargs={"k":3}
+    )
+
+    docs = retriever.invoke(question)
+
+    return docs
