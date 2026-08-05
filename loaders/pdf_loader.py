9b1299cc618024f0bb01a05aae12d2e6a911dcca
varshasu2023-prog <varshasu2023@gmail.com>
2026-08-05 11:39:02 +0530

    Update pdf_loader.py

--- loaders/pdf_loader.py
+++ loaders/pdf_loader.py
@@ -1,1 +1,28 @@
+import fitz  # PyMuPDF
 
+class PDFLoader:
+    def __init__(self, pdf_path):
+        self.pdf_path = pdf_path
+
+    def extract_text(self):
+        text = ""
+
+        try:
+            pdf = fitz.open(self.pdf_path)
+
+            for page in pdf:
+                text += page.get_text()
+
+            pdf.close()
+
+        except Exception as e:
+            print("Error:", e)
+
+        return text
+
+
+if __name__ == "__main__":
+    loader = PDFLoader("data/products.pdf")
+    document = loader.extract_text()
+
+    print(document[:1000])
