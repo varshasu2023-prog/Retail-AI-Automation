eac9b13fd839d7f4ce7715d7104f7417e4f52489
varshasu2023-prog <varshasu2023@gmail.com>
2026-08-05 14:56:57 +0530

    Update rag_agent.py

--- agents/rag_agent.py
+++ agents/rag_agent.py
@@ -4,30 +4,11 @@ import streamlit as st
 
 def generate_answer(vector_db, question):
 
-    docs = vector_db.similarity_search(question, k=3)
-
-    context = "\n\n".join(
-        [doc.page_content for doc in docs]
-    )
-
     llm = ChatGoogleGenerativeAI(
-        model="gemini-2.5-flash-lite",
-        google_api_key=st.secrets["GEMINI_API_KEY"],
-        temperature=0
+        model="gemini-2.5-flash",
+        google_api_key=st.secrets["GEMINI_API_KEY"]
     )
 
-    prompt = f"""
-You are a retail document assistant.
-
-Answer only using this context:
-
-{context}
-
-Question:
-{question}
-"""
-
-    response = llm.invoke(prompt)
+    response = llm.invoke("Explain retail in one sentence")
 
     return response.content
-
