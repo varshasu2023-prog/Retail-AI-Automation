41467cb3f30b2f3a6442fe0145e13a2d450af0ea
varshasu2023-prog <varshasu2023@gmail.com>
2026-08-05 13:39:56 +0530

    Update chroma_db.py

--- database/chroma_db.py
+++ database/chroma_db.py
@@ -6,13 +6,14 @@ import streamlit as st
 def create_vector_db(chunks):
 
     embeddings = GoogleGenerativeAIEmbeddings(
-        model="models/embedding-001",
+        model="models/text-embedding-004",
         google_api_key=st.secrets["GEMINI_API_KEY"]
     )
 
     vector_db = Chroma.from_documents(
         documents=chunks,
-        embedding=embeddings
+        embedding=embeddings,
+        persist_directory="chroma_db"
     )
 
     return vector_db
