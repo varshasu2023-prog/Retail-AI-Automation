405d9281aa664eff818584e6ee230bc6f6892664
varshasu2023-prog <varshasu2023@gmail.com>
2026-08-05 12:28:20 +0530

    Update and rename embeddings.py to text_splitter.py

---
+++ utils/text_splitter.py
@@ -0,0 +1,11 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+def split_text(text):
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+
+    chunks = splitter.create_documents([text])
+
+    return chunks
