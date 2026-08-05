859f415a40f360b407de86d046c2caeb535b5f20
varshasu2023-prog <varshasu2023@gmail.com>
2026-08-05 11:39:50 +0530

    Update embeddings.py

--- utils/embeddings.py
+++ utils/embeddings.py
@@ -1,1 +1,13 @@
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+embedding_model = GoogleGenerativeAIEmbeddings(
+    model="models/embedding-001",
+    google_api_key=os.getenv("GOOGLE_API_KEY")
+)
+
+def get_embedding():
+    return embedding_model
